6640d931c303dad910983a62e201553137f1c155
ILDANAXY <rd.dream@yandex.ru>
2023-03-27 10:33:23 +0500

    done

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,41 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.*;
+import java.util.function.BiConsumer;
+
+public class Pair<T, V> {
+    private final T firstValue;
+    private final V secondValue;
+    private Pair(T firstValue, V secondValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
+    public T getFirst() {
+        return firstValue;
+    }
+
+    public V getSecond() {
+        return secondValue;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) obj;
+        return this.firstValue.equals(pair.firstValue) && this.secondValue.equals(pair.secondValue);
+    }
+
+    public int hashCode() {
+        return Objects.hash(this.firstValue, this.secondValue);
+    }
+
+    public static <T, V> Pair<T, V> of(T firstValue, V secondValue){
+        return new Pair(firstValue, secondValue);
+    }
+
+    public void ifPresent(BiConsumer<? super T, ? super V> consumer){
+        if (this.firstValue != null && this.secondValue != null) {
+            consumer.accept((T)this.firstValue, (V)this.secondValue);
+        }
+    }
 }
--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -5,15 +5,20 @@ import java.io.InputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-
-        // TODO С корректно реализованным классом Pair должен компилироваться и успешно работать следующий код:
-
-        /*
         Pair<Integer, String> pair = Pair.of(1, "hello");
-        Integer i = pair.getFirst(); // 1
+        Number i = pair.getFirst(); // 1
         String s = pair.getSecond(); // "hello"
 
+        Pair<Number, String> pair3 = Pair.of(1, "str");
+        Number is = pair.getFirst(); // 1
+        String str = pair.getSecond(); // "hello"
+
         pair.ifPresent((first, second) -> {
+            System.out.println(first);
+            System.out.println(second);
+        });
+
+        pair3.ifPresent((first, second) -> {
             System.out.println(first);
             System.out.println(second);
         });
@@ -21,8 +26,6 @@ public class Task01Main {
         Pair<Integer, String> pair2 = Pair.of(1, "hello");
         boolean mustBeTrue = pair.equals(pair2); // true!
         boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
-        */
-
     }
 
 }
